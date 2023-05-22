2bc99d37becf11fe2dcc0cbcb5def267b088747e
gprocida <gprocida@xeniaprogetti.it>
2023-05-22 12:02:36 +0200

    build gender buttons and age edit text

--- app/src/main/java/com/example/plicometria/MainActivity.java
+++ app/src/main/java/com/example/plicometria/MainActivity.java
@@ -3,14 +3,50 @@ package com.example.plicometria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+  Button btnMale,btnFemale;
+  EditText etAge;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    //test git
+    initUI();
+    buttonsListener();
+    disabledAllField();
+  }
+
+  private void initUI() {
+    btnMale = findViewById(R.id.btn_male);
+    btnFemale = findViewById(R.id.btn_female);
+    etAge = findViewById(R.id.et_age);
+  }
+
+  private void disabledAllField() {
+    etAge.setEnabled(false);
+  }
+
+  private void buttonsListener() {
+    btnMale.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        btnFemale.setEnabled(false);
+        etAge.setEnabled(true);
+      }
+    });
+
+    btnFemale.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        btnMale.setEnabled(false);
+        etAge.setEnabled(true);
+      }
+    });
   }
 }
